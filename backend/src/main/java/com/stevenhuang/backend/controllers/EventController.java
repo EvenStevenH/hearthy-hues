b7842beeaacfbab243fbf2230543c262107c38a7
Steven Huang <22431150+EvenStevenH@users.noreply.github.com>
2026-09-10 15:59:23 -0400

    add path comments

--- backend/src/main/java/com/stevenhuang/backend/controllers/EventController.java
+++ backend/src/main/java/com/stevenhuang/backend/controllers/EventController.java
@@ -16,21 +16,25 @@ public class EventController {
         this.eventRepository = eventRepository;
     }
 
+    // GET all > http://127.0.0.1:8080/api/events
     @GetMapping
     public List<Event> getAllEvents() {
         return eventRepository.findAll();
     }
 
+    // GET one > http://127.0.0.1:8080/api/events/{id}
     @GetMapping("/{id}")
     public Event getEventById(@PathVariable int id) {
         return eventRepository.findById(id).orElse(null);
     }
 
+    // POST > http://127.0.0.1:8080/api/events
     @PostMapping
     public Event createEvent(@RequestBody Event event) {
         return eventRepository.save(event);
     }
 
+    // PUT > http://127.0.0.1:8080/api/events/{id}
     @PutMapping("/{id}")
     public Event updateEvent(@PathVariable int id, @RequestBody Event event) {
         if (eventRepository.existsById(id)) {
@@ -40,6 +44,7 @@ public class EventController {
         return null;
     }
 
+    // DELETE > http://127.0.0.1:8080/api/events/{id}
     @DeleteMapping("/{id}")
     public void deleteEvent(@PathVariable int id) {
         eventRepository.deleteById(id);
--- backend/src/main/java/com/stevenhuang/backend/controllers/OrganizerController.java
+++ backend/src/main/java/com/stevenhuang/backend/controllers/OrganizerController.java
@@ -16,21 +16,25 @@ public class OrganizerController {
         this.organizerRepository = organizerRepository;
     }
 
+    // GET all > http://127.0.0.1:8080/api/organizers
     @GetMapping
     public List<Organizer> getAllOrganizers() {
         return organizerRepository.findAll();
     }
 
+    // GET one > http://127.0.0.1:8080/api/organizers/{id}
     @GetMapping("/{id}")
     public Organizer getOrganizerById(@PathVariable int id) {
         return organizerRepository.findById(id).orElse(null);
     }
 
+    // POST > http://127.0.0.1:8080/api/organizers
     @PostMapping
     public Organizer createOrganizer(@RequestBody Organizer organizer) {
         return organizerRepository.save(organizer);
     }
 
+    // PUT > http://127.0.0.1:8080/api/organizers/{id}
     @PutMapping("/{id}")
     public Organizer updateOrganizer(@PathVariable int id, @RequestBody Organizer organizer) {
         if (organizerRepository.existsById(id)) {
@@ -40,6 +44,7 @@ public class OrganizerController {
         return null;
     }
 
+    // DELETE > http://127.0.0.1:8080/api/organizers/{id}
     @DeleteMapping("/{id}")
     public void deleteOrganizer(@PathVariable int id) {
         organizerRepository.deleteById(id);
