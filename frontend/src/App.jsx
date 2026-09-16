f115c96eae5040cc50a36d20f5f5bbd948aa7522
Steven Huang <22431150+EvenStevenH@users.noreply.github.com>
2026-09-16 11:21:47 -0400

    update app with new event routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import UserPage from "./components/user/UserPage.jsx";
 import EventsPage from "./components/events/EventsPage.jsx";
 import EventDetails from "./components/events/EventDetails.jsx";
 import AnimatedRoutes from "./components/AnimatedRoutes.jsx";
+import EventFormPage from "./components/events/EventFormPage.jsx";
 import { AnimatePresence } from "framer-motion";
 import { EventsProvider } from "./utils/EventsContext.jsx";
 import { Routes, Route, Navigate, useLocation } from "react-router";
@@ -45,8 +46,16 @@ export default function App() {
 											element={<EventsPage />}
 										/>
 										<Route
+											path="/events/new"
+											element={<EventFormPage />}
+										/>
+										<Route
 											path="/events/:eventId"
 											element={<EventDetails />}
+										/>
+										<Route
+											path="/events/:eventId/edit"
+											element={<EventFormPage />}
 										/>
 										<Route
 											path="/user"
