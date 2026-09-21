b900d5d240f8e5eb9930350e0431d2eed1db8a5c
Steven Huang <22431150+EvenStevenH@users.noreply.github.com>
2026-09-21 00:50:18 -0400

    update randomizer to use local storage

--- frontend/src/components/dashboard/Randomizer.jsx
+++ frontend/src/components/dashboard/Randomizer.jsx
@@ -1,16 +1,28 @@
 import { useState } from "react";
 import { getRandomIdea, getRandomHSL } from "../../utils/ideaUtils";
-import { FaRandom } from "react-icons/fa";
+import { useLocalStorage } from "../../utils/hooks";
+import { FaRandom, FaBookmark } from "react-icons/fa";
 
 export default function Randomizer({ subjects }) {
-	const [idea, setIdea] = useState(getRandomIdea(subjects)); // random idea on load
-	const [colors, setColors] = useState(getRandomHSL()); // random harmony on load
+	const [idea, setIdea] = useState(getRandomIdea(subjects));
+	const [colors, setColors] = useState(getRandomHSL());
+	const [savedIdeas, setSavedIdeas] = useLocalStorage("savedIdeas", []);
+	const [savedHarmonies, setSavedHarmonies] = useLocalStorage("savedHarmonies", []);
 
 	const getIdea = () => {
 		setIdea(getRandomIdea(subjects));
 	};
+
 	const getHarmony = () => {
 		setColors(getRandomHSL());
+	};
+
+	const saveIdea = () => {
+		setSavedIdeas((prev) => [...prev, idea]);
+	};
+
+	const saveHarmony = () => {
+		setSavedHarmonies((prev) => [...prev, colors]);
 	};
 
 	return (
@@ -24,9 +36,22 @@ export default function Randomizer({ subjects }) {
 						<b>Subject</b>: {idea.subject}
 					</p>
 				</div>
-				<button onClick={getIdea}>
-					<FaRandom /> Get a new subject!
-				</button>
+
+				<div className="randomizerButtons">
+					<button
+						onClick={getIdea}
+						id="randomSubjectBtn"
+					>
+						New subject! <FaRandom />
+					</button>
+
+					<button
+						onClick={saveIdea}
+						id="saveSubjectBtn"
+					>
+						Save <FaBookmark />
+					</button>
+				</div>
 			</div>
 
 			<hr />
@@ -35,6 +60,7 @@ export default function Randomizer({ subjects }) {
 				<p>
 					<b>Color Harmony</b>: {colors.harmony}
 				</p>
+
 				<ul className="randomHarmony">
 					{colors.colors.map((color, index) => (
 						<li
@@ -44,9 +70,22 @@ export default function Randomizer({ subjects }) {
 						></li>
 					))}
 				</ul>
-				<button onClick={getHarmony}>
-					<FaRandom /> Get a new color harmony!
-				</button>
+
+				<div className="randomizerButtons">
+					<button
+						onClick={getHarmony}
+						id="randomHarmonyBtn"
+					>
+						New color harmony! <FaRandom />
+					</button>
+
+					<button
+						onClick={saveHarmony}
+						id="saveHarmonyBtn"
+					>
+						Save <FaBookmark />
+					</button>
+				</div>
 			</div>
 		</>
 	);
